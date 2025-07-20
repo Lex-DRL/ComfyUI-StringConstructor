12d61600e72e51c9e090477306631f69035930eb
Lex Darlog (DRL) <3897975+Lex-DRL@users.noreply.github.com>
2025-07-21 01:44:23 +0500

    `__init__.py`

---
+++ __init__.py
@@ -0,0 +1,10 @@
+# encoding: utf-8
+"""
+"""
+
+import typing as _t
+
+NODE_CLASS_MAPPINGS: _t.Dict[str, type] = dict()
+NODE_DISPLAY_NAME_MAPPINGS: _t.Dict[str, str] = dict()
+
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
